4a061da1cd7a3f2be3e56bddbecce3d4502a545e
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-25 15:25:01 -0300

    feature(type_menu): indentação type menu actions

--- src/type_options_menu.sh
+++ src/type_options_menu.sh
@@ -4,59 +4,49 @@
 function type_menu() {
 
     case "${ACESSO}" in
-        1)
-            docker_version # function docker_version localizada em docker_list.sh (Função para pegar a versão do docker)
+        1) docker_version # function docker_version localizada em docker_list.sh (Função para pegar a versão do docker)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        2)
-            docker_container_list # function docker_container_list localizada em docker_list.sh (Função para listar os container em execução)
+        2) docker_container_list # function docker_container_list localizada em docker_list.sh (Função para listar os container em execução)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        3)
-            docker_image_list # function docker_image_list localizada em docker_list.sh (Função para listar as imagens docker)
+        3) docker_image_list # function docker_image_list localizada em docker_list.sh (Função para listar as imagens docker)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        4)
-            docker_volume_list # function docker_volume_list localizada em docker_list.sh (Função para listar os volumes)
+        4) docker_volume_list # function docker_volume_list localizada em docker_list.sh (Função para listar os volumes)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        5)
-            docker_network_list # function docker_network_list localizada em docker_list.sh (Função para listar os  networks)
-    echo "=================================================="
-    ;;
-        6)
-            docker_container_remove # function docker_container_remove localizada em docker_remove.sh (Função para remover os containers)
+        5) docker_network_list # function docker_network_list localizada em docker_list.sh (Função para listar os  networks)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        7)
-            docker_image_remove # function docker_image_remove localizada em docker_remove.sh (Função para remover as imagens)
-    echo "=================================================="
-    ;;
-        8)
-            docker_volume_remove # function docker_volume_remove localizada em docker_remove.sh (Função para remover os volumes)
-    echo "=================================================="
-    ;;
-        9)
-            docker_network_remove # function docker_network_remove localizada em docker_remove.sh (Função para remover os networks)
+        6) docker_container_remove # function docker_container_remove localizada em docker_remove.sh (Função para remover os containers)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        10)
-            menu_docker_run # function docker_run localizada em docker_run.sh (Função para criar containers)
+        7) docker_image_remove # function docker_image_remove localizada em docker_remove.sh (Função para remover as imagens)
+        echo "=================================================="
+        ;;
 
-    echo "=================================================="
-    ;;
-        0)
-            sair # function sair localizada em em docker_list.sh (Função para sair do sistema)
+        8) docker_volume_remove # function docker_volume_remove localizada em docker_remove.sh (Função para remover os volumes)
+        echo "=================================================="
+        ;;
 
-    echo "================================================"
-    ;;
+        9) docker_network_remove # function docker_network_remove localizada em docker_remove.sh (Função para remover os networks)
+        echo "=================================================="
+        ;;
 
+        10) menu_docker_run # function docker_run localizada em docker_run.sh (Função para criar containers)
+        echo "=================================================="
+        ;;
+
+        0) sair # function sair localizada em em docker_list.sh (Função para sair do sistema)
+        echo "================================================"
+        ;;
     *)
     esac
 
