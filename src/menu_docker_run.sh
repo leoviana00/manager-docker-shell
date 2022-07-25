cb275742eea8db9dcd7fe23c2d7cb45df327c8e4
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-25 15:23:03 -0300

    feature(menu): add item voltar menu

--- src/menu_docker_run.sh
+++ src/menu_docker_run.sh
@@ -10,6 +10,7 @@ function menu_docker_run() {
     echo -e "${GREEN}==================================================${NC}"
     echo -e "${IYELLOW}1 - Não ${NC}"
     echo -e "${IYELLOW}2 - Sim ${NC}"
+    echo -e "${IYELLOW}3 - Voltar ao menu ${NC}"
     echo -e "${GREEN}==================================================${NC}"
     echo -e "${BWhite}Escolha uma das opções acima:  ${NC}"; read ACESSO
     echo -e "${BWhite}Opção informada ($ACESSO)${NC}"
