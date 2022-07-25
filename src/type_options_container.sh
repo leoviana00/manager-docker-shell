8807f5f35cd39a6be3d0a3056ef13367e672bc36
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-25 15:24:14 -0300

    update(type-container): indentação

--- src/type_options_container.sh
+++ src/type_options_container.sh
@@ -3,13 +3,17 @@
 # Função que contem a ação a ser chamada para cada opção de container a ser criado escolhido
 function type_container() {
     case "$ACESSO" in
-        1)
-            docker_run # Function docker_run, localizada em docker_run.sh (Função que cria um container)
-    echo "=================================================="
-    ;;
-        2)
-            docker_run_ext # Function docker_run, localizada em docker_run.sh (Função que cria um container com acesso web)
-    echo "=================================================="
-    ;;
+        1) docker_run # Function docker_run, localizada em docker_run.sh (Função que cria um container)
+        echo "=================================================="
+        ;;
+
+        2) docker_run_ext # Function docker_run, localizada em docker_run.sh (Função que cria um container com acesso web)
+        echo "=================================================="
+        ;;
+        
+        3) menu # Function docker_run, localizada em docker_run.sh (Função que cria um container com acesso web)
+        echo "=================================================="
+        ;;
+    *)
     esac
 }
