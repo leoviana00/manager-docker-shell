2cbeee10d3407cf5964e39ad8636c615ac9ecad3
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-25 14:32:42 -0300

    feature(manager-docker):  add arquivo main

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Importaçõe
+source ./src/colors.sh
+source ./src/docker_list.sh
+source ./src/docker_remove.sh
+source ./src/docker_run.sh
+source ./src/menu_docker_run.sh
+source ./src/log.sh
+source ./src/menu.sh
+source ./src/validacao_options_container.sh
+source ./src/validacao_options_menu.sh
+source ./src/type_options_container.sh
+source ./src/type_options_menu.sh
+
+
+# Forçar o script ser utilizado com user rootDiferença de EUID para UID?
+if [[ ${EUID} -ne 0 ]]; then
+    log "Esse script deve ser executado como root." > /dev/stderr
+    exit 1
+fi
+
+menu # Chamando a função menu em ./src/menu.sh
