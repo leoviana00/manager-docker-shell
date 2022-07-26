5912696059beb4b42beeafb47eef7db4bcb36e93
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-26 07:48:13 -0300

    feature(libs): movendo colors e logs para lib

--- lib/log.sh
+++ lib/log.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 # Função para implementar log ( echo com a hora)
+
+# Log - AVISO
 function log() {
   echo -e "${IYELLOW}$(date +'%Y-%m-%d %H:%M:%S')" "==> Manager Docker: $1${NC}"
+}
+
+# Log - ERROR
+function log_err() {
+  echo -e "${RED}$(date +'%Y-%m-%d %H:%M:%S')" "==> Error: $1${NC}"
 }
