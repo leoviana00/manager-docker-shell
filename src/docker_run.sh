d2662cd4e04b2e5484ed5b2500ca39d16a160772
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-26 07:50:28 -0300

    feature(docker-run): verifica se image base existe

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # Funções para criar containers
-docker_run(){
+docker_run() {
     log "Criando um contaneir ${Name} com imagem base ${ImageBase} ..."
     docker container run --name ${Name} -d ${ImageBase} >/dev/null 2>&1 # Criando o container
-    docker container ls --format "{{.Names}}:  {{.Ports}}" | grep ${Name} | awk -F  ":::" '{print $1}' | awk -F ":" '{OFS="\t";print $1,$3}' # Pegando o nome do container e porta
+    if [ $? -eq 0 ]; then
+        docker container ls --format "{{.Names}}:  {{.Ports}}" | grep ${Name} | awk -F  ":::" '{print $1}' | awk -F ":" '{OFS="\t";print $1,$3}' # Pegando o nome do container e porta
+    else
+        log_err "Imagem ${ImageBase} não encontrada, tente uma imagem base existente"
+    fi
     sleep 5
 }
 
-docker_run_ext(){
+docker_run_ext() {
     log "Criando um contaneir ${Name} com imagem base ${ImageBase} ..."
     docker container run -P --name ${Name} -d ${ImageBase} >/dev/null 2>&1 # Criando o container
-    docker container ls --format "{{.Names}}:  {{.Ports}}" | grep ${Name} | awk -F  ":::" '{print $1}' | awk -F ":" '{OFS="\t";print $1,$3}' # Pegando o nome do container e porta
-    PORT=`docker container ls --format "{{.Names}}:  {{.Ports}}" | grep ${Name} | awk -F  ":" '{print $3}' | awk -F "-" '{print $1}' ` # Pegando a porta para acesso externo do container e atribuindo em uma variável
-    echo -e "${BWhite} URL: http://localhost:${PORT}${NC}" # Passando a url para acesso via web ao serviço
+    if [ $? -eq 0 ]; then
+        docker container ls --format "{{.Names}}:  {{.Ports}}" | grep ${Name} | awk -F  ":::" '{print $1}' | awk -F ":" '{OFS="\t";print $1,$3}' # Pegando o nome do container e porta
+        PORT=`docker container ls --format "{{.Names}}:  {{.Ports}}" | grep ${Name} | awk -F  ":" '{print $3}' | awk -F "-" '{print $1}' ` # Pegando a porta para acesso externo do container e atribuindo em uma variável
+        echo -e "${BWhite} URL: http://localhost:${PORT}${NC}" # Passando a url para acesso via web ao serviço
+    else
+        log_err "Imagem ${ImageBase} não encontrada, tente uma imagem base existente"
+    fi
     sleep 5
 }
